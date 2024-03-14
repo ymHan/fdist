5abb25820f51d79700585a0631d8ae9e32d5312e
ymHan <onyxsard@gmail.com>
2024-03-14 11:32:55 +0900

    first commit

---
+++ shared/sdk/src/enum/account-roles.ts
@@ -0,0 +1,9 @@
+export enum AccountRoles {
+  ADMIN = 'admin',
+  SUPERVISOR = 'supervisor',
+  USER = 'user',
+  OPERATOR = 'operator',
+  MODERATOR = 'moderator',
+  MANAGER = 'manager',
+  MEMBER = 'member',
+}
